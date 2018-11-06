c0f14ba8a372c250aa2766043f620513e995cfdb
Oliver Faso <oli1243@live.com>
2018-11-06 11:04:25 -0500

    start of benchmark

--- Benchmark.swift
+++ Benchmark.swift
@@ -1,6 +1,11 @@
+import Foundation
+
+func getCurrentMillis() -> Int64 {
+	return Int64(NSDate().timeIntervalSince1970 * 1000)
+}
+
 class Benchmark {
 	var startTimeMillis: Int64 = 0
 	var endTimeMillis: Int64 = 0
 
-	
 }
