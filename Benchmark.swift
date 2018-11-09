92ec23f87705f63a13ce179361260e6e58800de1
user <oli1243@live.com>
2018-11-08 20:35:37 -0500

    BaseBenchmark and cross-platform rng

--- Benchmark.swift
+++ Benchmark.swift
@@ -4,8 +4,27 @@ func getCurrentMillis() -> Int64 {
 	return Int64(NSDate().timeIntervalSince1970 * 1000)
 }
 
-class Benchmark {
-	var startTimeMillis: Int64 = 0
-	var endTimeMillis: Int64 = 0
+func getRandomInt(range: Int) -> Int {
+	#if os(macOS)
+		return Int(arc4random_uniform(UInt32(range)))
+	#elseif os(Linux)
+		return Int(rand()) % range
+	#endif
+}
 
+class BaseBenchmark {
+	var startTimeMillis: Int64 = 0,
+	    endTimeMillis: Int64 = 0
+
+	var elapsedMillis: Int {
+		return Int(endTimeMillis - startTimeMillis)
+	}
+
+	func startTimer() {
+		startTimeMillis = getCurrentMillis()
+	}
+
+	func stopTimer() {
+		endTimeMillis = getCurrentMillis()
+	}
 }
